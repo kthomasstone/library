f3c109a68389f442f74b24798778f237ef424a2d
kthomasstone <22228480+kthomasstone@users.noreply.github.com>
2024-05-05 17:52:41 -0500

    Add "Remove book" buttons for each book.

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const bookshelf = document.querySelector(".bookshelf");
 const addBookForm = document.querySelector(".add-book-form");
 const fab = document.querySelector(".fab");
 const addBookBtn = document.getElementById("addBookBtn");
+const removeBookButton = document.getElementById("buttonRemoveBook");
 
 // on click
 // toggle the display of the form
@@ -29,14 +30,14 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return `
-    <p>${title}</p> 
-    <p>by ${author}</p>
-    <p>pages: ${pages}</p> 
-    <p>status: ${read}</p>`;
-  };
 }
+// Book.prototype.info = function () {
+//   return `
+//   <p class="title">${this.title}</p>
+//   <p class="author">by ${this.author}</p>
+//   <p class="pages">pages: ${this.pages}</p>
+//   <p class="read">status: ${this.read}</p>`;
+// };
 
 // ADD BOOK TO LIBRARY
 function addBookToLibrary(book) {
@@ -47,19 +48,44 @@ function addBookToLibrary(book) {
 function createBookCard(book) {
   // create container
   const newBookDiv = document.createElement("div");
-  // add object as content to newBookDiv variable
-  newBookDiv.innerHTML = book.info();
   // apply css class to style container
   newBookDiv.classList.add("book");
+
+  // Create elements for each book property
+  const titleBook = document.createElement("p");
+  const authorBook = document.createElement("p");
+  const pagesBook = document.createElement("p");
+  const readBook = document.createElement("p");
+
+  titleBook.classList.add("title");
+  authorBook.classList.add("author");
+  pagesBook.classList.add("pages");
+  readBook.classList.add("read");
+
+  titleBook.textContent = `${book.title}`;
+  authorBook.textContent = `by: ${book.author}`;
+  pagesBook.textContent = `${book.pages}`;
+  readBook.textContent = `${book.read}`;
+
+  newBookDiv.appendChild(titleBook);
+  newBookDiv.appendChild(authorBook);
+  newBookDiv.appendChild(pagesBook);
+  newBookDiv.appendChild(readBook);
+
+  const removeBookButton = createRemoveBookButton();
+  newBookDiv.appendChild(removeBookButton);
+
   return newBookDiv;
 }
 
 // BUILD LIBRARY IN HTML
 function buildLibrary(library, bookshelf) {
   // iterate over library array items
-  library.forEach((book) => {
+  library.forEach((book, index) => {
     // create a new book card
     const bookCard = createBookCard(book);
+    // assign data attribute equal to index number
+    bookCard.setAttribute("data-index", index);
     // add card to bookshelf
     bookshelf.appendChild(bookCard);
   });
@@ -73,7 +99,6 @@ addBookBtn.addEventListener("click", (event) => {
   // prompt the user to answer questions about the book's title, author, pages, and whether they've read it.
   // store prompt answer after each question
   const title = document.getElementById("title").value;
-  console.log(title);
   const author = document.getElementById("author").value;
   const pages = document.getElementById("pages").value;
   const read = document.getElementById("status").value;
@@ -86,5 +111,47 @@ addBookBtn.addEventListener("click", (event) => {
 
   // Create and append the new book card to the existing bookshelf content
   const bookCard = createBookCard(newBook);
+  // set data-index attribute for new book card
+  bookCard.setAttribute("data-index", library.length - 1);
   bookshelf.appendChild(bookCard);
 });
+
+// CREATE REMOVE BOOK BUTTON
+function createRemoveBookButton() {
+  // create button
+  const buttonRemoveBook = document.createElement("button");
+  buttonRemoveBook.classList.add("remove-book");
+
+  const buttonStateLayer = document.createElement("div");
+  buttonStateLayer.classList.add("btn-state-layer");
+
+  const buttonTextP = document.createElement("p");
+  buttonTextP.textContent = "Remove book";
+  buttonTextP.classList.add("buttonText");
+
+  buttonStateLayer.appendChild(buttonTextP);
+  buttonRemoveBook.appendChild(buttonStateLayer);
+
+  return buttonRemoveBook;
+}
+
+// add event listener to the bookshelf
+bookshelf.addEventListener("click", (event) => {
+  if (event.target.closest(".remove-book")) {
+    const bookCard = event.target.closest(".book");
+    if (bookCard) {
+      bookshelf.removeChild(bookCard);
+      const bookCardIndex = bookCard.getAttribute("data-index");
+      library.splice(bookCardIndex, 1);
+      // repopulate library bookCard indices
+      updateBookIndices();
+    }
+  }
+});
+
+function updateBookIndices() {
+  const remainingBooks = document.querySelectorAll(".book");
+  remainingBooks.forEach((book, index) => {
+    book.setAttribute("data-index", index);
+  });
+}
